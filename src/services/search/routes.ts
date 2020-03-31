4938d0018e84a06e76c7cf8e07093bbdb2191917
Max Pijittum <mahasak@gmail.com>
2020-04-01 00:59:35 +0800

    add search controller & service with sample cache handler

--- src/services/search/routes.ts
+++ src/services/search/routes.ts
@@ -1,11 +1,24 @@
 import { Request, Response } from "express";
+import { getPlacesByName } from "./SearchController";
+import { cache } from "../../middleware/cache";
 
 export default [
-  {
-    path: "/",
-    method: "get",
-    handler: async (req: Request, res: Response) => {
-      res.send("Hello world!");
+    {
+        path: "/",
+        method: "get",
+        handler: async (req: Request, res: Response) => {
+            res.send("Hello world!");
+        }
+    },
+    {
+        path: "/api/v1/search",
+        method: "get",
+        handler: [
+            cache, // Checking cache
+            async ({ query }: Request, res: Response) => {
+                const result = await getPlacesByName(query.q);
+                res.status(200).send(result);
+            }
+        ]
     }
-  }
 ];
